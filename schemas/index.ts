da4b7b1f038ba70fd78f94690922122dceeed81d
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2024-02-15 18:25:31 -0300

    segmento 6 Register Form

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,15 @@ export const LoginSchema = z.object({
     message: "Password is required"
   })
 }) 
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email"
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required"
+  }),
+  name: z.string().min(4, {
+    message: "Name minimum 4 characters required"
+  })
+}) 
