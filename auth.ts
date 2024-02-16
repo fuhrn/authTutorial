c8074114361e3cf8a324c3c559c7a2a4b93018a8
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2024-02-16 18:50:26 -0300

    segmento 10mo Callbacks

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
+import { UserRole} from "@prisma/client";
 import { PrismaAdapter} from "@auth/prisma-adapter";
 
+import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 
@@ -10,6 +12,35 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) { 
+      // console.log({
+      //   sessionToken: token,
+      // });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      } 
+
+      if (token.role && session.user) { }
+      session.user.role = token.role as UserRole;
+
+      return session;
+    },
+    // genero el token de acceso para luego tenerlo disponible en el objeto Request
+    // y de ahi ver si el user es admin o no
+    async jwt({ token }) {
+      // si no sub significa estoy logout
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
