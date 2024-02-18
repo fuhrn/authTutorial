192277db76fcf4e7f54422713a349c6a885a63c6
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2024-02-18 19:11:49 -0300

    segmento 17: User Botton

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,28 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-const SettingPage = async () => {
-  const session = await auth();
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+// client component....
+import { useSession, signOut } from "next-auth/react";
+// server actions...
+// import { logout } from "@/actions/logout";
+
+const SettingPage = () => {
+  // const session = useSession();
+  const user = useCurrentUser()
+
+  const onClick = () => {
+    signOut();
+    // logout()
+  }
 
   return (
-    <div>
-      Setting Page
-      {JSON.stringify(session)}
-      <form action={
-        async () => {
-          "use server"
-          await signOut();
-        }
-      }>
-        <button type="submit">
+    <div className="bg-white p-10 rounded-xl">
+      {/* {JSON.stringify(session.data?.user)} */}
+      {/* {JSON.stringify(user)} */}
+      <button onClick={ onClick } type="submit">
           Sign Out
         </button>
-      </form>
     </div>
   )
 }
